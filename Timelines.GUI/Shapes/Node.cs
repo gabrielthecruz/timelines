c25ff4f7ea633696be37b3dfccb3f5ab596cf853
Gabriel Cruz <gabriel.cruz232@gmail.com>
2025-11-05 02:02:40 -0300

    rendering single-child nodes

---
+++ Timelines.GUI/Shapes/Node.cs
@@ -0,0 +1,151 @@
+﻿using System.Diagnostics.Metrics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Timelines.Core.Exceptions;
+
+namespace Timelines.GUI.Shapes
+{
+	public class Node
+	{
+		internal static Canvas? DefaultCanvas;
+
+		private Node? Parent;
+		private string _title = string.Empty;
+		private double _x = 0;
+		private double _y = 0;
+
+		public Ellipse Shape;
+		public Label Label;
+		public Guid Id { get; }
+		public string Title 
+		{ 
+			get { return _title; }
+			set
+			{
+				_title = value;
+				Label.Content = value;
+				Label.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+				Canvas.SetLeft(Label, X - (Label.ActualWidth / 2) + (Shape.Width / 2));
+			}
+		}
+		public double X
+		{
+			get { return _x; }
+			set
+			{
+				_x = value;
+				Canvas.SetLeft(Shape, value);
+				Canvas.SetLeft(Label, value - (Label.ActualWidth / 2) + (Shape.Width / 2));
+				if (Parent is not null)
+					Parent.Lines[Id].X2 = value + Shape.Width / 2;
+
+				foreach (var (_, line) in Lines)
+				{
+					line.X1 = value + Shape.Width / 2;
+				}
+			}
+		}
+		public double Y
+		{
+			get { return _y; }
+			set
+			{
+				_y = value;
+				Canvas.SetTop(Shape, value);
+				Canvas.SetTop(Label, value + 10);
+				if (Parent is not null)
+					Parent.Lines[Id].Y2 = value + Shape.Height / 2;
+
+				foreach (var (_, line) in Lines)
+				{
+					line.Y1 = value + Shape.Height / 2;
+				}
+			}
+		}
+
+		public Dictionary<Guid, Node> Children { get; set; }
+		public Dictionary<Guid, Line> Lines { get; set; }
+
+		public Node()
+		{
+			Parent = null;
+			Id = Guid.NewGuid();
+			Lines = [];
+			Children = [];
+
+			Shape = new Ellipse
+			{
+				Opacity = 100,
+				HorizontalAlignment = HorizontalAlignment.Center,
+				VerticalAlignment = VerticalAlignment.Center,
+				Width = 10,
+				Height = 10,
+				Stroke = Brushes.Black,
+				Fill = Brushes.Black
+			};
+			
+			DefaultCanvas?.Children.Add(Shape);
+
+			Label = new Label
+			{
+				Opacity = 100,
+				HorizontalAlignment = HorizontalAlignment.Center,
+				VerticalAlignment = VerticalAlignment.Center,
+				FontSize = 10,
+				Content = _title,
+				Target = Shape
+			};
+
+			DefaultCanvas?.Children.Add(Label);
+			Title = string.Empty;
+		}
+
+		public void AddChild(Node child)
+		{
+			if (Children.ContainsKey(child.Id))
+			{
+				throw new ChildAlreadyExistsException("Cannot add existent child to node.");
+			}
+
+			Children.Add(child.Id, child);
+			child.Parent = this;
+			
+			if (!Lines.ContainsKey(child.Id))
+			{
+				Lines.Add(child.Id, new Line
+				{
+					Opacity = 100,
+					Stroke = Brushes.Black,
+					X1 = X + Shape.Width / 2,
+					Y1 = Y + Shape.Height / 2,
+					X2 = child.X + child.Shape.Width / 2,
+					Y2 = child.Y + child.Shape.Height / 2
+				});
+				
+				DefaultCanvas?.Children.Add(Lines[child.Id]);
+			}
+		}
+
+		public void RemoveChild(Guid nodeId)
+		{
+			if (!Children.ContainsKey(nodeId))
+			{
+				throw new MissingChildException($"Node '{nodeId}' is not a child of '{Title}'.");
+			}
+
+			Children.Remove(nodeId);
+		}
+
+		public void Render(double x, double y, double dist = 50)
+		{
+			X = x; Y = y;
+			foreach (var (_, child) in Children)
+			{
+				child.Render(x + dist, y);
+			}
+		}
+
+	}
+}
---
+++ Timelines.GUI/Windows/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Timelines.GUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.N:
+					var newProjectWindow = new ProjectWindow();
+					newProjectWindow.Show();
+                    this.Close();
+                    break;
+                case Key.O:
+                    MessageBox.Show("Open Project!\nNot implemented yet!");
+                    break;
+                case Key.Q:
+                    this.Close();
+                    break;
+            }
+
+            e.Handled = true;
+        }
+	}
+}
