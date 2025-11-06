942c0b440579f674dad816f31365e867644d54a2
Gabriel Cruz <gabriel.cruz232@gmail.com>
2025-11-05 23:13:32 -0300

    removed WPF project and Core project. add win forms project.

--- Timelines.Core/Exceptions/ChildAlreadyExistsException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Timelines.Core.Exceptions
-{
-	public class ChildAlreadyExistsException : Exception
-	{
-		public ChildAlreadyExistsException() { }
-		public ChildAlreadyExistsException(string message) : base(message) { }
-		public ChildAlreadyExistsException(string message, Exception inner) : base(message, inner) { }
-	}
-}
--- Timelines.Core/Exceptions/MissingChildException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Timelines.Core.Exceptions
-{
-	public class MissingChildException : Exception
-	{
-		public MissingChildException() { }
-		public MissingChildException(string message) : base(message) { }
-		public MissingChildException(string message, Exception inner) : base(message, inner) { }
-	}
-}
